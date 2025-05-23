f34dcef4a9fc7c90fe758778a9759555d0095219
JP <prasadjadhav@hotmail.com>
2025-05-23 16:54:05 +0530

    Add .next/static/chunks/src_app_how-it-works_page_tsx_a3165332._.js 1747999447349

---
+++ .next/static/chunks/src_app_how-it-works_page_tsx_a3165332._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_eddf9ae4._.js"
+],
+    source: "dynamic"
+});
